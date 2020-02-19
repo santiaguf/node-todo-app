133f58e8149d8ca7fec7c153da9b57a276af36a6
Santiago Bernal B <exactlimon@gmail.com>
2020-02-19 01:12:39 -0500

    server and db config

---
+++ config/database.config.js
@@ -0,0 +1,4 @@
+//URL string connection
+module.exports = {
+    url: 'mongodb://localhost:27017/todoapp'
+}
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// create express app
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to Todo App."});
+});
+
+// Require Notes routes
+require('./app/routes/user.routes.js')(app);
+
+// listen for requests
+app.listen(3005, () => {
+    console.log("Server is listening on port 3005");
+});
