71111ded69a582b837710dd36ed2675d8102b313
Santiago Bernal B <exactlimon@gmail.com>
2020-02-19 01:49:36 -0500

    find user

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -36,3 +36,25 @@ exports.findAll = (req, res) => {
         });
     });
 };
+
+// Find a single user with a userId
+exports.findOne = (req, res) => {
+    User.findById(req.params.userId)
+    .then(user => {
+        if(!user) {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.userId
+            });            
+        }
+        res.send(user);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.userId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving user with id " + req.params.userId
+        });
+    });
+};
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -7,8 +7,8 @@ module.exports = (app) => {
     // Retrieve all Users
     app.get('/users', users.findAll);
 
-    // // Retrieve a single User with userId
-    // app.get('/users/:userId', users.findOne);
+    // Retrieve a single User with userId
+    app.get('/users/:userId', users.findOne);
 
     // // Update a User with userId
     // app.put('/users/:userId', users.update);
