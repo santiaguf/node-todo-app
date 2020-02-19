26f739d335f22479f866e393ad792b52d702c30b
Santiago Bernal B <exactlimon@gmail.com>
2020-02-19 01:20:03 -0500

    model

---
+++ model/user.model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    name: String
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', UserSchema);
